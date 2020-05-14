5a631b10b14d371e01693bce1fb4bd3708f1070d
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-05-14 16:12:15 +0530

    Implemented decorater function

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,21 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+std::string Format::ElapsedTime(long seconds) {
+
+    const int SECPERMIN =60;
+    const int SECPERHOUR =3600;
+
+    long lseconds = seconds % SECPERMIN;
+    long lminutes = (seconds / SECPERMIN) %SECPERMIN;
+    long lhours   = seconds /SECPERHOUR;
+
+    std::stringstream ssresult;
+
+    ssresult << std::setfill('0') << std::setw(2) << lhours << ":";
+    ssresult << std::setfill('0') << std::setw(2) << lminutes << ":";
+    ssresult << std::setfill('0') << std::setw(2) << lseconds;
+
+    return ssresult.str();
+}
